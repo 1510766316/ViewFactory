02ef93f917839e50ad04216cf79a985ec8779caa
guangxi.wu <guangxi.wu@imotorinfo.com>
2017-08-02 15:16:45 +0800

    add a test

---
+++ app/src/main/java/com/wgx/love/viewFactory/CVF.java
@@ -0,0 +1,26 @@
+package com.wgx.love.viewFactory;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by imotor on 17-8-2.
+ */
+
+public class CVF extends View{
+
+    public CVF(Context context) {
+        super(context);
+    }
+
+    public CVF(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    
+    public CVF(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
